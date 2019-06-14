17e1e7773c9354afdde1e3f108aa198def86d8bf
ahmedfouad <lionmoon60@yahoo.com>
2019-06-14 21:00:55 +0200

    #100DaysOfFlutter Day04 json parsing with built_value [the boringShow] .. EP03

--- BoringFlutter show/Pilot Episode/hn_app/lib/json_parsing.dart
+++ BoringFlutter show/Pilot Episode/hn_app/lib/json_parsing.dart
@@ -1,18 +1,107 @@
-import 'package:hn_app/src/article.dart';
-import 'dart:convert' as json;
 
-List<int> parseTopStories(String jsonString){
 
-  final parsed = json.jsonDecode(jsonString);
-  final listOfIds=List<int>.from(parsed);
+//// GENERATED CODE - DO NOT MODIFY BY HAND
+//
+//part of serializers;
+//
+//// **************************************************************************
+//// BuiltValueGenerator
+//// **************************************************************************
+//
+//Serializers _$serializers = (new Serializers().toBuilder()
+//  ..add(Article.serializer)
+//  ..addBuilderFactory(
+//      const FullType(BuiltList, const [const FullType(int)]),
+//          () => new ListBuilder<int>())
+//  ..addBuilderFactory(
+//      const FullType(BuiltList, const [const FullType(int)]),
+//          () => new ListBuilder<int>()))
+//    .build();
+//
+//// ignore_for_file: always_put_control_body_on_new_line,always_specify_types,annotate_overrides,avoid_annotating_with_dynamic,avoid_as,avoid_catches_without_on_clauses,avoid_returning_this,lines_longer_than_80_chars,omit_local_variable_types,prefer_expression_function_bodies,sort_constructors_first,test_types_in_equals,unnecessary_const,unnecessary_new
 
-  return listOfIds;
-}
 
-Article parseArticle(String jsonStr){
 
-  final parsed= json.jsonDecode(jsonStr);  //Decode the Json
-  Article article=Article.fromJson(parsed);
-  return article;
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//import 'package:hn_app/src/article.dart';
+//import 'dart:convert' as json;
+//import 'package:built_value/built_value.dart';
+//import 'package:built_collection/built_collection.dart';
+//import 'package:built_value/serializer.dart';
+//
+//import 'package:hn_app/src/serializers.dart';
+//part 'json_parsing.g.dart';
+//
+//abstract class Article implements Built<Article, ArticleBuilder> {
+//
+//    static Serializer<Article> get serializer => _$articleSerializer;
+//
+//  int get id;
+//
+//  @nullable
+//  bool get deleted;
+//
+//  String get type;
+//
+//  String get by;
+//
+//  int get time;
+//
+//  @nullable
+//  String get text;
+//
+//  @nullable
+//  bool get dead;
+//
+//  @nullable
+//  int get poll;
+//
+//  BuiltList<int> get kids;
+//
+//  @nullable
+//  String get url;
+//
+//  @nullable
+//  int get score;
+//
+//  @nullable
+//  String get title;
+//
+//  BuiltList<int> get parts;
+//
+//  @nullable
+//  int get descendants;
+//
+//  Article._();
+//
+//  factory Article([void Function(ArticleBuilder) updates]) = _$Article;
+//}
+//
+//List<int> parseTopStories(String jsonString) {
+//  return []; // return List<int>();
+//
+////  final parsed = json.jsonDecode(jsonString);
+////  final listOfIds=List<int>.from(parsed);
+////
+////  return listOfIds;
+//}
+//
+//Article parseArticle(String jsonStr) {
+//  final parsed= json.jsonDecode(jsonStr);  //Decode the Json
+//  Article article=standardSerializers.deserializeWith(Article.serializer, parsed);
+//  return article;
+//}
--- BoringFlutter show/Pilot Episode/hn_app/lib/main.dart
+++ BoringFlutter show/Pilot Episode/hn_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'json_parsing.dart';
 import 'src/article.dart';
 import "package:url_launcher/url_launcher.dart";
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -28,7 +32,29 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  List <Article> _articles = articles;
+//  List <Article> _articles = articles;
+
+List<int>_ids=[
+  18052923,
+  18053337,
+  18034912,
+  18046274,
+  18041368,
+  18054574,
+  18039489,
+  18050090,
+  18047418,
+  18035283
+];
+
+//return future Articles
+  Future<Article>_getArticle(int id ) async{
+    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
+    final storyRes = await http.get(storyUrl);
+    if (storyRes.statusCode == 200) {
+      return parseArticle(storyRes.body);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,18 +64,19 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: new RefreshIndicator(
-          onRefresh: () async{
-            await new Future.delayed(const Duration(seconds: 1));
-            setState(() {
-              _articles.removeAt(0);
-            });
+        child:ListView(children: _ids.map((i)=>
+            FutureBuilder<Article>(
+              future: _getArticle(i),
+              builder: (BuildContext context,AsyncSnapshot<Article>snapshot){
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return _buildItem(snapshot.data);
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            )
 
-          },
-          child: ListView(
-//        mainAxisAlignment: MainAxisAlignment.center,
-            children: _articles.map( _buildItem).toList()
-          ),
+        ).toList(),
         ),
       ),
      // This trailing comma makes auto-formatting nicer for build methods.
@@ -60,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
 //    if(e.text.startsWith("Circular")) return null;
         return Padding(
           padding: const EdgeInsets.all(8.0),
-          child: new ExpansionTile(title: new Text(article.text,style: TextStyle(fontSize: 19),),
+          child: new ExpansionTile(title: new Text(article.title?? '[null]',style: TextStyle(fontSize: 19),),
 //            subtitle: new Text("${article.commentsCount} Comments"),
 //            onTap: () async {
 //                final fakeUrl="http://${article.domain}";
@@ -71,15 +98,15 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                new Text("${article.commentsCount} Comments"),
+                new Text("${article.type}"),
                 new MaterialButton(color: Colors.deepOrange,
                   textColor: Colors.white,
 
                   onPressed:() async{
 
-                      final fakeUrl="http://${article.url}";
-                      if (await canLaunch(fakeUrl)){
-                        launch(fakeUrl);
+//                      final fakeUrl="http://${article.url}";
+                      if (await canLaunch(article.url)){
+                        launch(article.url);
                       }
                       
 
